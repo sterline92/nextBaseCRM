935d97139800541e7f0ce1679d0aeeeaab88a87d
NazarPshebishevskyi <74192973+NazarPshebishevskyi@users.noreply.github.com>
2021-01-28 14:50:15 -0500

    testing my updates

--- src/main/java/Hello.java
+++
@@ -1,9 +0,0 @@
-package day26_Arrays2;
-
-import java.util.Arrays;
-
-public class ArraysUtility {
-
-    public static void main(String[] args){
-    }
-}
--- src/test/java/Nazar/TC001.java
+++ src/test/java/Nazar/TC001.java
@@ -1,4 +1,7 @@
 package Nazar;
 
 public class TC001 {
+    public static void main(String[] args) {
+        System.out.println("What's going on group 15? ");
+    }
 }

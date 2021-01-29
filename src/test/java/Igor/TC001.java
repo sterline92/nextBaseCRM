af2588ce30a54922e4e999da5c25b020d7e2c0f9
Igor Turko <ihorturko@gmail.com>
2021-01-29 14:03:36 -0500

    added new note

--- src/test/java/Igor/TC001.java
+++ src/test/java/Igor/TC001.java
@@ -4,5 +4,6 @@ public class TC001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //adding a note
+        //adding new note to the branch
     }
 }

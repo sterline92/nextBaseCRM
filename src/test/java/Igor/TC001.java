512789ce4c7024522fe9dac6f70e2d7ef99bf4c4
Igor Turko <ihorturko@gmail.com>
2021-01-29 14:05:02 -0500

    added 2nd note

--- src/test/java/Igor/TC001.java
+++ src/test/java/Igor/TC001.java
@@ -4,6 +4,6 @@ public class TC001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //adding a note
-        //adding new note to the branch
+        //adding 2nd note
     }
 }
